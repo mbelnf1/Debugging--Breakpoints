7d2e3826d7fdbd2bc0f873910a9af1618053d281
mbelnf1 <mbeln1992@gmail.com>
2019-08-12 18:04:44 -0400

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+
+            p.FirstName = "Timothy";
+            p.LastName = "Corey";
+
+            Console.WriteLine(p.FunkyNameBuilder());
+
+            Console.ReadLine();
+        }
+    }
+}
